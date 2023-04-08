3684f3eec590aec974b9be0794a7a25211f9c561
Admin <sofiia.vynar.ir.2022@lpnu.ua>
2023-04-08 18:22:31 +0300

    Initial Commit

---
+++ src/ua/lviv/iot/algo/part1/lab1/Drone.java
@@ -0,0 +1,25 @@
+package ua.lviv.iot.algo.part1.lab1;
+
+import lombok.*;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(callSuper = true)
+public class Drone extends Aircraft{
+    private int batteryCapacity;
+    private int energyConsumptionPerMinute;
+    public Drone(String manufacturer, int maxSpeed, int batteryCapacity, int energyConsumptionPerMinute){
+        super (manufacturer, maxSpeed);
+        this.batteryCapacity = batteryCapacity;
+        this.energyConsumptionPerMinute = energyConsumptionPerMinute;
+    }
+    @Override
+    public int getMaxFlyingDistance() {
+        return (int) (batteryCapacity/energyConsumptionPerMinute * maxSpeed);
+    }
+    @Override
+    public int getMaxDeliveryWeight() {
+        return 0;
+    }
+}
