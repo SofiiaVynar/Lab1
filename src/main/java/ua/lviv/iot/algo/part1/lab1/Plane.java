558b0e75128b272669ce35bda6e278f0a34fe6b1
Admin <sofiia.vynar.ir.2022@lpnu.ua>
2023-04-08 18:14:05 +0300

    Initial Commit

---
+++ src/main/java/ua/lviv/iot/algo/part1/lab1/Plane.java
@@ -0,0 +1,22 @@
+package ua.lviv.iot.algo.part1.lab1;
+
+import lombok.ToString;
+@ToString(callSuper = true)
+public class Plane extends Aircraft {
+    public static final double halfWeight = 1.6;
+    private final int wingsSpan;
+    private final int weight;
+    public Plane(String manufacturer, int maxSpeed, int wingsSpan, int weight) {
+        super(manufacturer, maxSpeed);
+        this.wingsSpan = wingsSpan;
+        this.weight = weight;
+    }
+    @Override
+    public int getMaxFlyingDistance() {
+        return (int) (Math.pow(getMaxSpeed(), 2) / 20 * wingsSpan);
+    }
+    @Override
+    public int getMaxDeliveryWeight() {
+        return (int) (halfWeight * weight);
+    }
+}
